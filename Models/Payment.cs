45578b50eb5a48a7a2adcd14cca338dafad66bb3
Rori <roriiisang@gmail.com>
2026-07-10 12:35:38 +0200

    feat(payments): add core payments API with EF Core

---
+++ Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿using PaymentOrchestrator_Lite_BE.Models.Enums;
+
+namespace PaymentOrchestrator_Lite_BE.Models
+{
+    public class Payment
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string CustomerId { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public PaymentStatus Status { get; set; } = PaymentStatus.Pending; // Pending, Confirmed
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+    public record CreatePaymentRequest(string CustomerId, decimal Amount);
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
+using PaymentOrchestrator_Lite_BE.Data;
+using PaymentOrchestrator_Lite_BE.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,10 +27,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Payments API v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
